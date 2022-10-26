ea65576fa4feb9cde05a420a6003f58e51e06d42
prabhat mishra <115269832+Prabhatmishra7131@users.noreply.github.com>
2022-10-26 14:56:45 +0530

    Add files via upload

---
+++ Q10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("Table of 5:\n");
+    for (i = 5; i <= 50; i += 5)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
---
+++ Q8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("Square of first ten natural no.s is\n");
+    for ( i = 0; i <= 9; i++)
+    
+    {
+        printf("%d\n",i*i);
+    }
+    return 0;
+}
